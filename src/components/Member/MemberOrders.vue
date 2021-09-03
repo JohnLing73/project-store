3af666843ff664810e740ac7b678dfd422ec3a49
JohnLing <dudulala73@gmail.com>
2021-09-03 20:34:13 +0800

    commit

--- src/components/Member/MemberOrders.vue
+++ src/components/Member/MemberOrders.vue
@@ -1,58 +1,83 @@
 <template>
-  <table v-for="info in order" :key="info.orderId">
-    <thead>
-      <tr>
-        <td>Shipping Way</td>
-        <td>Order Date</td>
-        <td>Order Number</td>
-        <td>Quantities</td>
-        <td>Total Price</td>
-        <td>Pay By</td>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>{{ info.shippingWay }}</td>
-        <td>{{ info.orderDate }}</td>
-        <td>{{ info.orderId }}</td>
-        <td>{{ orderTotalAmount }}</td>
-        <td>{{ orderTotalPrice }}</td>
-        <td>{{ info.pay }}</td>
-      </tr>
-    </tbody>
-  </table>
+  <div class="each-table">
+    <table v-for="info in order" :key="info.orderId">
+      <thead>
+        <tr>
+          <th>Shipping Way</th>
+          <th>Order Date</th>
+          <th>Order Number</th>
+          <th>Quantities</th>
+          <th>Total Price</th>
+          <th>Pay By</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>{{ info.shippingWay }}</td>
+          <td>{{ info.orderDate }}</td>
+          <td>{{ info.orderId }}</td>
+          <td>{{ orderTotalAmount }}</td>
+          <td>{{ orderTotalPrice }}</td>
+          <td>{{ info.pay }}</td>
+        </tr>
+      </tbody>
+    </table>
+    <base-button :link='false'>Check Detail</base-button>
+  </div>
 </template>
 <script>
 export default {
-  computed:{
+  computed: {
     order() {
-      return this.$store.state.member.theFinalMembers[0].orders
+      return this.$store.state.member.theFinalMembers[0].orders;
     },
     orderTotalAmount() {
       var amount = 0;
-      for(let i = 0; i < this.order[0].orderItems.length; i++) {
+      for (let i = 0; i < this.order[0].orderItems.length; i++) {
         amount += this.order[0].orderItems[i].productQuantity;
       }
       return amount;
     },
     orderTotalPrice() {
       var sum = 0;
-      for(let i = 0; i < this.order[0].orderItems.length; i++) {
-          sum += ( this.order[0].orderItems[i].productQuantity ) * ( this.order[0].orderItems[i].productEachPrice );
+      for (let i = 0; i < this.order[0].orderItems.length; i++) {
+        sum +=
+          this.order[0].orderItems[i].productQuantity *
+          this.order[0].orderItems[i].productEachPrice;
       }
       return sum;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style lang="scss">
-  table {
-    border-collapse: collapse;
-  }
-  table,
-  tr,
-  td {
-    border: 1px solid black;
-    text-align: center;
-  }
+.each-table {
+  width: 750px;
+  margin: auto;
+  border-top: 1px dashed black;
+  border-bottom: 1px dashed black;
+  padding-top: 1rem;
+}
+table {
+  border-collapse: collapse;
+  width: 100%;
+}
+
+table,
+th,
+td {
+  border: 1px solid black;
+  text-align: center;
+  padding: 0.5rem 0.3rem;
+}
+
+th {
+  background-color: $base-button-bck;
+  padding: 0.5rem 0.5rem;
+  color: $white;
+}
+
+button {
+  margin: 1rem 0;
+}
 </style>
--- src/pages/member/MemberPage.vue
+++ src/pages/member/MemberPage.vue
@@ -1,89 +1,105 @@
 <template>
-  <div class="member-page" :class="{darkMode: this.$store.state.normal.colorTheme === 'light'}">
+  <div
+    class="member-page"
+    :class="{ darkMode: this.$store.state.normal.colorTheme === 'light' }"
+  >
     <div class="member-info">
       <h2>Member Center</h2>
-      <p>Member Center >> {{ tab }} </p>
+      <p>Member Center >> {{ tab }}</p>
     </div>
     <div class="member-side-bar">
-      <button class="member-tab-btn" @click="switchTab('MemberProfile')">Member Profile</button>
-      <button class="member-tab-btn" @click="switchTab('MemberOrders')">Member Orders</button>
-      <button class="member-tab-btn" @click="switchTab('MemberCart')">Member Cart</button>
-      <button class="member-tab-btn" @click="switchTab('MemberWishlists')">Member Wishlists</button>
+      <button
+        v-for="(tab, idx) in tabs"
+        :key="idx"
+        class="member-tab-btn"
+        :class="{ selecting: this.tab === tab }"
+        @click="switchTab(this.tabs[idx])"
+      >
+        {{ tab }}
+      </button>
     </div>
     <div class="member-main-part">
       <keep-alive>
-        <component :is='tab'></component>
+        <component :is="tab"></component>
       </keep-alive>
     </div>
   </div>
 </template>
 <script>
-import MemberOrders from '../../components/Member/MemberOrders.vue';
-import MemberCart from '../../components/Member/MemberCart.vue';
-import MemberWishlists from '../../components/Member/MemberWishlists.vue';
-import MemberProfile from '../../components/Member/MemberProfile.vue';
+import MemberOrders from "../../components/Member/MemberOrders.vue";
+import MemberCart from "../../components/Member/MemberCart.vue";
+import MemberWishlists from "../../components/Member/MemberWishlists.vue";
+import MemberProfile from "../../components/Member/MemberProfile.vue";
 export default {
-  components:{
+  components: {
     MemberOrders,
     MemberCart,
     MemberWishlists,
-    MemberProfile
+    MemberProfile,
   },
   data() {
     return {
-      tab: 'MemberOrders'
-    }
+      tab: "MemberOrders",
+      tabs: ["MemberProfile", "MemberOrders", "MemberCart", "MemberWishlists"],
+    };
   },
-  methods:{
+  methods: {
     switchTab(tab) {
       this.tab = tab;
-    }
-  }
-}
+    },
+  },
+};
 </script>
 <style lang="scss" scoped>
-  .member-page {
-    @include flex-model($wrap: wrap) ;
-    padding: $distance-window ;
-    background-color: $product-main-bg;
-    width: 1200px;
-    margin: auto;
-  }
+.member-page {
+  @include flex-model($wrap: wrap);
+  padding: $distance-window;
+  background-color: $product-main-bg;
+  width: 1200px;
+  margin: auto;
+}
 
-  .member-page.darkMode {
-    background-color: $product-dark-mode !important;
-  }
+.member-page.darkMode {
+  background-color: $product-dark-mode !important;
+}
 
-  .member-main-part {
-    width: calc(100% - 197px);
-  }
+.member-main-part {
+  width: calc(100% - 197px);
+}
 
-  .member-info {
-    flex-basis: 100%;
-      h2 {
-        text-align: center;
-      }
-      p {
-        padding-left: 170px;
-      }
+.member-info {
+  flex-basis: 100%;
+  h2 {
+    text-align: center;
   }
+  p {
+    padding-left: 170px;
+  }
+}
 
-  .member-side-bar {
-    @include flex-model($dir: column);
-  }
+.member-side-bar {
+  @include flex-model($dir: column);
+}
 
-  .member-tab-btn {
-    padding: 0.7rem 0.3rem;
-    font-size: 1rem;
-    margin-bottom: 1rem;
-    font-family: 'Play', sans-serif;
-    background-color: $base-button-bck;
-    color: $white;
-    display: inline-block;
-    margin-right: $distance-window;
-  }
-   .card {
-    margin: 0 auto !important;
-    width: 700px;
-  }
+.member-tab-btn {
+  padding: 0.7rem 0.3rem;
+  font-size: 1rem;
+  margin-bottom: 1rem;
+  font-family: "Play", sans-serif;
+  background-color: $base-button-bck;
+  color: $white;
+  display: inline-block;
+  margin-right: $distance-window;
+}
+.card {
+  margin: 0 auto !important;
+  width: 750px;
+}
+
+button {
+  transition: all 0.5s ease;
+}
+.selecting {
+  background-color: #488a5a;
+}
 </style>
